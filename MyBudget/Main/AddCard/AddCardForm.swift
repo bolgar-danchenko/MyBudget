50b09e576637592d1d1bf52db976cd9d6df795da
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2023-01-30 23:20:04 +0400

    AddCardForm UI ready

--- MyBudget/Main/AddCard/AddCardForm.swift
+++ MyBudget/Main/AddCard/AddCardForm.swift
@@ -11,10 +11,60 @@ struct AddCardForm: View {
     
     @Environment(\.dismiss) private var dismiss
     
+    @State private var name = ""
+    @State private var cardNumber = ""
+    @State private var limit = ""
+    @State private var cardType = "Visa"
+    
+    @State private var month = 1
+    @State private var year = Calendar.current.component(.year, from: Date())
+    
+    @State private var color = Color.blue
+    
+    let currentYear = Calendar.current.component(.year, from: Date())
+    
     var body: some View {
         NavigationView {
-            ScrollView {
-                Text("Add card form")
+            Form {
+                Section {
+                    TextField("Name", text: $name)
+                    
+                    TextField("Credit Card Number", text: $cardNumber)
+                        .keyboardType(.numberPad)
+                    
+                    TextField("Credit Limit", text: $limit)
+                        .keyboardType(.numberPad)
+                    
+                    Picker("Type", selection: $cardType) {
+                        ForEach(["Visa", "MasterCard", "Discovery", "Citibank"], id: \.self) { cardType in
+                            Text(String(cardType)).tag(String(cardType))
+                        }
+                    }
+                } header: {
+                    Text("Card Info")
+                }
+                
+                Section {
+                    Picker("Month", selection: $month) {
+                        ForEach(1...12, id: \.self) { num in
+                            Text(String(num)).tag(String(num))
+                        }
+                    }
+                    
+                    Picker("Year", selection: $year) {
+                        ForEach(currentYear..<currentYear + 20, id: \.self) { num in
+                            Text(String(num)).tag(String(num))
+                        }
+                    }
+                } header: {
+                    Text("Expiration")
+                }
+                
+                Section {
+                    ColorPicker("Color", selection: $color)
+                } header: {
+                    Text("Color")
+                }
             }
             .navigationTitle("Add Credit Card")
             .toolbar {
