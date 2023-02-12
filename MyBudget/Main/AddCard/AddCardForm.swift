97284fa27fe0237362f3dd7ae3eb73fe0760e1c0
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2023-02-12 09:42:33 +0400

    Fixed bug with empty card type

--- MyBudget/Main/AddCard/AddCardForm.swift
+++ MyBudget/Main/AddCard/AddCardForm.swift
@@ -23,7 +23,7 @@ struct AddCardForm: View {
             _limit = State(initialValue: String(limit))
         }
         
-        _cardType = State(initialValue: self.card?.cardType ?? "")
+        _cardType = State(initialValue: self.card?.cardType ?? "Visa")
         _month = State(initialValue: Int(self.card?.expMonth ?? 1))
         _year = State(initialValue: Int(self.card?.expYear ?? Int16(currentYear)))
         
@@ -85,6 +85,7 @@ struct AddCardForm: View {
             card.expYear = Int16(self.year)
             card.timestamp = Date()
             card.color = UIColor(self.color).encode()
+            
             card.cardType = self.cardType
             
             do {
